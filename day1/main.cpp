d085cf9dde2673c4f571c554a2995592598b00a1
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2023-12-03 13:07:45 -0800

    more work on part 2

--- day1/main.cpp
+++ day1/main.cpp
@@ -2,12 +2,13 @@
  * DISCLAIMER: This is the very first C++ program that I've ever written
  */
 
+#include <exception>
 #include <iostream>
 #include <vector>
 #include <cctype>
-#include <string>
 #include <fstream>
 #include <unordered_map>
+#include <optional>
 
 
 class Calibration { 
@@ -48,50 +49,54 @@ class Calibration {
             return sum;
         }
 
-        std::vector<std::string> fixForPart2(std::vector<std::string> calibrations) {
-            std::vector<std::string> fixedCalibrations {};
-
-            for(std::string calibration: calibrations) { 
-                std::string replacement = replaceNumericalStrings(calibration);
-                std::cout << "Replaced: " << calibration << " with: " << replacement << std::endl;
-                fixedCalibrations.push_back(replacement);
-            }
-
-            return fixedCalibrations;
+        int part2(std::vector<std::string> input) {
+            return 0;
         }
 
-        std::string replaceNumericalStrings(std::string input) {
-            std::unordered_map<std::string, std::string> replacements {};
-            replacements["one"] = "1";
-            replacements["two"] = "2";
-            replacements["three"] = "3";
-            replacements["four"] = "4";
-            replacements["five"] = "5";
-            replacements["six"] = "6";
-            replacements["seven"] = "7";
-            replacements["eight"] = "8";
-            replacements["nine"] = "9";
+        std::string firstNumberInString(std::string) {
+            int substringSize = 0;
+            //TOOD: iterate through string, taking larger and larger substring slices until numberInString returns nonempty optional
+            return "";
+        }
 
-            std::string replacement = input;
-            for(const auto& replacementPair: replacements) {
-                auto key = replacementPair.first;
-                auto value = replacementPair.second;
 
-                auto position = replacement.find(key);
-                if(position != std::string::npos) {
-                    replacement = replacement.replace(position, key.length(), value);
+        std::optional<std::string> numberInString(std::string input) {
+            std::unordered_map<std::string, std::string> numbers {};
+            numbers["0"] = "0";
+            numbers["1"] = "1";
+            numbers["2"] = "2";
+            numbers["3"] = "3";
+            numbers["4"] = "4";
+            numbers["5"] = "5";
+            numbers["6"] = "6";
+            numbers["7"] = "7";
+            numbers["8"] = "8";
+            numbers["9"] = "9";
+            numbers["one"] = "1";
+            numbers["two"] = "2";
+            numbers["three"] = "3";
+            numbers["four"] = "4";
+            numbers["five"] = "5";
+            numbers["six"] = "6";
+            numbers["seven"] = "7";
+            numbers["eight"] = "8";
+            numbers["nine"] = "9";
+            for(std::pair<std::string, std::string> numberPair: numbers) {
+                auto foundNumberIndex = input.find(numberPair.first);
+                if(foundNumberIndex != std::string::npos) {
+                    return std::optional(numberPair.second);
                 }
-                //position = replacement.find_last_of(key);
-                //if(position != std::string::npos) {
-                    //replacement = replacement.replace(position, key.length(), value);
-                //}
             }
-            return replacement;
+            return std::nullopt;
         }
 };
 
 int main(int argc, char* argv[]) {
     std::cout << "Starting program" << std::endl;
+    if (argc < 2) {
+        std::cout << "No argument" << std::endl;
+        return -1;
+    }
     std::string inputFileName {argv[1]};
     std::ifstream inputFile(inputFileName);
     std::vector<std::string> rawInput {};
@@ -106,9 +111,8 @@ int main(int argc, char* argv[]) {
     inputFile.close();
     Calibration calibration {};
 
-    std::vector<std::string> inputCleanedForPart2 = calibration.fixForPart2(rawInput);
-
-    calibration.FindCalibration(inputCleanedForPart2);
+    int calibrationPart2 = calibration.part2(rawInput);
+    std::cout << "The final calibration value is: " << calibrationPart2 << std::endl;
 
     std::cout << "End of program" << std::endl;
     return 0;
